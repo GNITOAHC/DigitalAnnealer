3159ce433907990a267dba1998980f1c4f9c9a7e
GNITOAHC <chaotingchen10@gmail.com>
2024-01-15 21:08:04 +0800

    refactor: Add gamma swapping back to program

--- src/mpi/Annealer.cc
+++ src/mpi/Annealer.cc
@@ -264,12 +264,12 @@ double Annealer::annealGamma(const std::tuple<double, double>& gamma, Graph& gra
 
         // std::cout << "anneal in mpi" << std::endl;
 
-        if (false) {
+        if (i % 8 == 0) {
             // MPI swap spins by temperature and hamiltonian energy
             std::vector<Spin> config = graph.getSpins();
-            printf("Config before swap: ");
-            for (int i = 0; i < config.size(); ++i)
-                printf("%d ", config[i]);
+            // printf("Config before swap: ");
+            // for (int i = 0; i < config.size(); ++i)
+            //     printf("%d ", config[i]);
             std::vector<double> vertical_energy_list = graph.getVerticalEnergyProduct(std::get<1>(graph_size));
             double vertical_energy_sum = 0.0;
             for (const auto& energy : vertical_energy_list) {
@@ -278,10 +278,10 @@ double Annealer::annealGamma(const std::tuple<double, double>& gamma, Graph& gra
 
             // MPI communication
             gammaSwap(this->myrank, gamma, vertical_energy_sum, config);
-            printf("Config: ");
-            for (int i = 0; i < config.size(); ++i)
-                printf("%d ", config[i]);
-            printf("\n");
+            // printf("Config: ");
+            // for (int i = 0; i < config.size(); ++i)
+            //     printf("%d ", config[i]);
+            // printf("\n");
         }
     }
     return graph.getHamiltonianEnergy();
