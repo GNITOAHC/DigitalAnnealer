abc40d3d8285f608173805c7fb6608f047f915ad
GINTOAHC <chaotingchen10@gmail.com>
2024-06-04 14:21:17 +0800

    refactor: Remove test files

--- c.sh
+++
@@ -1,5 +0,0 @@
-make && \
-echo "----------MAKE COMPLETE----------" && \
-./main_exe --file ./sample/sample3.in --init-g 0.2 --tau 100 --finil-g 0.0 --func sqa && \
-echo "----------EXECUTION COMPLETE-----" && \
-bat tmp*
